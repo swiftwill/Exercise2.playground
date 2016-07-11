8ff6c4a4ff906437148e2558a61cdfee33182d59
swiftwill <prc-lh138-18@PRC-LH138-18s-iMac.local>
2016-07-11 14:12:38 -0500

    Exercise 2
    Usage of constants and variables

---
+++ Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, Pokie!"
+
+let p2 = str
+
+let s:String = "Where ya goin?"
+
+let i:Int = 21
+
+let f:Float = 3.14
+
+let d:Double = 99.99
+
+let b:Bool = true
